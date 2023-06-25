32272eaf10428b060f3e7a03a535e6ce5eafb3d5
himanganimesh2 <himanganimesh2@gmail.com>
2023-06-25 06:33:39 +0530

    video content park is done

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -154,7 +154,7 @@ const Menu = ({darkMode, setDarkMode}) => {
             </Item>
             <Item onClick={()=> setDarkMode(!darkMode)}> 
                 <SettingsBrightnessIcon/>
-                Light Mode
+                {darkMode ? "Light" : "Dark"} Mode
             </Item>
         </Wrapper>
     </Container>
--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
+import ReplyOutlinedIcon from '@mui/icons-material/ReplyOutlined';
+import AddTaskOutlinedIcon from '@mui/icons-material/AddTaskOutlined';
 
 const Container = styled.div`
   display:flex;
@@ -15,19 +19,33 @@ const VideoWrapper = styled.div`
   
 `
 const Title = styled.h1`
-
+  font-size:18px;
+  font-weight:400;
+  margin: 20px 0 10px 0;
+  color: ${({theme}) => theme.text}
 `
 const Details = styled.div`
-  
+  display: flex;
+  align-items: center;
+  justify-content:space-between;
 `
 const Info = styled.span`
-  
+color: ${({theme}) => theme.textSoft}
 `
 const Buttons = styled.div`
-  
+  display:flex;
+  gap:20px;
+  color: ${({theme}) => theme.text} 
 `
 const Button = styled.div`
-  
+  display:flex;
+  align-items:center;
+  gap:5px;
+  cursor:pointer;
+`
+const Hr = styled.hr`
+  margin: 15px 0;
+  border: 0.5px solid ${({theme}) => theme.soft}; 
 `
 
 const Video = () => {
@@ -38,7 +56,7 @@ const Video = () => {
           <iframe
             width="100%"
             height="720"
-            src='https://www.youtube.com/watch?v=ei6nKsXXMbY'
+            src='https://www.youtube.com/embed/ei6nKsXXMbY'
             title='Youtube video player'
             frameborder="0"
             allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
@@ -49,9 +67,13 @@ const Video = () => {
         <Details>
           <Info>456,641 views June 22, 2023</Info>
           <Buttons>
-            
+            <Button><ThumbUpOffAltIcon/> 123</Button>
+            <Button><ThumbDownOffAltIcon/> Dislike</Button>
+            <Button><ReplyOutlinedIcon/> Share</Button>
+            <Button><AddTaskOutlinedIcon/> Save</Button>
           </Buttons>
         </Details>
+        <Hr/>
       </Content>
       <Recomandation>Recomandation</Recomandation>
     </Container>
